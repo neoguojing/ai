576df444af110fa9b4e57137c1760f89355f8f11
guoerjun <guoerjun@sensetime.com>
2022-10-14 19:03:08 +0800

    up

--- .ipynb_checkpoints/model_tools-checkpoint.py
+++ .ipynb_checkpoints/model_tools-checkpoint.py
@@ -5,6 +5,9 @@ import onnxruntime as ort
 from importlib import import_module
 import cv2
 import numpy as np
+import os
+import features
+pwd = os.getcwd()
 
 #model1 = torch.load("hybridnets.pth",map_location=torch.device('cpu'))
 
@@ -15,12 +18,11 @@ import numpy as np
 
 app = typer.Typer()
 
-model_dir = '/data/ai/models/'
 input1 = torch.randn(1,3,640,384)
 
 @app.command()
-def run_pytorch_model(path: str, gpu: bool = False):
-    model = load_model(path,gpu)
+def run_pytorch_model(path: str):
+    model = load_model(path)
     if model == None:
         return
     output_list = model(img)
@@ -29,8 +31,8 @@ def run_pytorch_model(path: str, gpu: bool = False):
     
     
 @app.command()
-def run_onnx_model(path: str, gpu: bool = False):
-    model = load_model(path,gpu,'onnx')
+def run_onnx_model(path: str):
+    model = load_model(path,'onnx')
     ort_session = ort.InferenceSession(path) # 创建一个推理session
     outputs = ort_session.run(None,{ 'input_0' : input1})
     print(outputs)
@@ -40,8 +42,8 @@ def valid_check(output1: object,output2: object):
     np.testing.assert_allclose(output1.cpu().numpy(), output2[0], rtol=1e-03, atol=1e-05)
     
 @app.command()
-def pth_to_onnx(path: str,gpu: bool = False):
-    model = load_model(path,gpu)
+def pt_to_onnx(path: str):
+    model = load_model(path)
     if model == None:
         return
     
@@ -51,34 +53,39 @@ def pth_to_onnx(path: str,gpu: bool = False):
     onnx_model = torch.onnx.export(model,input1,path+'onnx',input_names=input_names,output_names=output_names)
     onnx.checker.check_model(onnx_model)
     
-def load_model(path: str,gpu: bool,where: str = 'pytorch'):
+def load_model(path: str,where: str = 'jit'):
     model = object()
     if where == 'jit':
-        if gpu:
-            use_gpu = torch.cuda.is_available()
-            if not use_gpu:
-                print("unsupport gpu")
-                return None
-            input1 = input1.cuda()
+        use_gpu = torch.cuda.is_available()
+        if use_gpu:
             model = torch.jit.load(path)
             # 关闭用于训练和评估的Dropouts（防止过拟合），BatchNorm（归一化，加速训练）
             model.eval().cuda()
         else:
             model = torch.jit.load(path,map_location=torch.device('cpu'))
             model.eval()
-    else if where == 'pytorch':
+    elif where == 'pytorch':
         checkpoint = torch.load(PATH)
         print(checkpoint)
         model.load_state_dict(checkpoint['model_state_dict'])
-    else if where == 'onnx':
+    elif where == 'onnx':
         model = onnx.load(path) # 加载onnx
-        onnx.checker.check_model(model) # 检查生成模型是否错误
+        if not onnx.checker.check_model(model): # 检查生成模型是否错误
+            return None
     return model
-    
+
 @app.command()
-def info(path: str,gpu: bool = False,where: str = 'pytorch'):
-    if where == 'pytorch':
-        model = load_model(path,gpu,where)
+def save_model(name: str = 'resnet18',dst: str = pwd ):
+    model = features.get_feature_modes()
+    model_scripted = torch.jit.script(model) # Export to TorchScript
+    name = os.path.join(dst,name+'.pt')
+    print(name)
+    model_scripted.save(name) # Save
+     
+@app.command()
+def info(path: str,where: str = 'jit'):
+    if where != 'jit':
+        model = load_model(path,where)
         if model == None:
             return
 
@@ -89,7 +96,12 @@ def info(path: str,gpu: bool = False,where: str = 'pytorch'):
         for  layer in model:
             x = layer(input1)
             print(layer.__class__.__name__,"output shape:",x.shape)
-
+            
+def print_model_info(model: object):
+    # print('input shape:',model.inpu)
+    # print('input size:',model.size)
+    print('params:',model.parameters())
+    
 def image_format(path: str,size,shape):
     img = cv2.imread(path)
     img = cv2.resize(img,size)
@@ -101,7 +113,11 @@ def download(git: str,name: str,where: str = 'pytorch'):
     if where == "pytorch":
         model = torch.hub.load(git, name, pretrained=True)
         model_scripted = torch.jit.script(model) # Export to TorchScript
-        model_scripted.save(model_dir+name+'.pt') # Save
+        model_scripted.save(pwd+name+'.pt') # Save
     
+@app.command()
+def test():
+    model = features.get_feature_modes()
+    print_model_info(model)
 if __name__ == '__main__':
     app()
--- features.py
+++ features.py
@@ -1,11 +1,12 @@
+import torch
 import torchvision.models as models
 import torchvision.transforms as transforms
 from PIL import Image
 
 
-def get_feature_modes(gpu bool: True):
+def get_feature_modes():
     model = models.resnet18()
-    input_image = Image.open(filename)
+    input_image = Image.open('/data/ai/ai/pexels-pixabay-45201.jpg')
     preprocess = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
@@ -17,13 +18,17 @@ def get_feature_modes(gpu bool: True):
 
     # move the input and model to GPU for speed if available
     if torch.cuda.is_available():
-        input_batch = input_batch.to('cuda')
-        model.to('cuda')
+        input_batch = input_batch.cuda()
+        model.cuda()
 
     with torch.no_grad():
         output = model(input_batch)
     # Tensor of shape 1000, with confidence scores over Imagenet's 1000 classes
-    print(output[0])
+    print('out:',output[0])
     # The output has unnormalized scores. To get probabilities, you can run a softmax on it.
     probabilities = torch.nn.functional.softmax(output[0], dim=0)
-    print(probabilities)
+    print('probabilities:',probabilities)
+    return model
+
+def get_feature_input():
+    torch.randn(1,3,640,384)

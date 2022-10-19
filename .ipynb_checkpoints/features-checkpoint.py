8798a12fa99c6d21cd3884f4b4a5f1a165bea120
guoerjun <guoerjun@sensetime.com>
2022-10-19 20:47:37 +0800

    up

--- .ipynb_checkpoints/features-checkpoint.py
+++ .ipynb_checkpoints/features-checkpoint.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 
 def get_feature_modes():
-    model = models.resnet18()
+    model = models.resnet18(pretrained=True)
     input_image = Image.open('/data/ai/ai/pexels-pixabay-45201.jpg')
     preprocess = transforms.Compose([
         transforms.Resize(256),
@@ -19,16 +19,31 @@ def get_feature_modes():
     # move the input and model to GPU for speed if available
     if torch.cuda.is_available():
         input_batch = input_batch.cuda()
-        model.cuda()
+        model.eval().cuda()
 
-    with torch.no_grad():
-        output = model(input_batch)
+    # with torch.no_grad():
+    #     output = model(input_batch)
     # Tensor of shape 1000, with confidence scores over Imagenet's 1000 classes
-    print('out:',output[0])
+    # print('out:',output[0])
     # The output has unnormalized scores. To get probabilities, you can run a softmax on it.
-    probabilities = torch.nn.functional.softmax(output[0], dim=0)
-    print('probabilities:',probabilities)
+    # probabilities = torch.nn.functional.softmax(output[0], dim=0)
+    # print('probabilities:',probabilities)
     return model
 
 def get_feature_input():
-    torch.randn(1,3,640,384)
+    model = models.resnet18()
+    input_image = Image.open('/data/ai/ai/pexels-pixabay-45201.jpg')
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    input_tensor = preprocess(input_image)
+    input_batch = input_tensor.unsqueeze(0)
+    if torch.cuda.is_available():
+        input_batch = input_batch.cuda()
+    print(input_tensor)
+    print(input_batch)
+    return input_batch
+    
--- .ipynb_checkpoints/model_tools-checkpoint.py
+++ .ipynb_checkpoints/model_tools-checkpoint.py
@@ -7,6 +7,10 @@ import cv2
 import numpy as np
 import os
 import features
+import torchvision.models as models
+from PIL import Image
+from google.protobuf.json_format import MessageToDict
+
 pwd = os.getcwd()
 
 #model1 = torch.load("hybridnets.pth",map_location=torch.device('cpu'))
@@ -25,16 +29,19 @@ def run_pytorch_model(path: str):
     model = load_model(path)
     if model == None:
         return
-    output_list = model(img)
+    input1 = features.get_feature_input()
+    output_list = model(input1)
     print(output_list)
     
+
     
     
 @app.command()
 def run_onnx_model(path: str):
-    model = load_model(path,'onnx')
-    ort_session = ort.InferenceSession(path) # 创建一个推理session
-    outputs = ort_session.run(None,{ 'input_0' : input1})
+    load_model(path=path,where='onnx')
+    ort_session = ort.InferenceSession(path,providers=['TensorrtExecutionProvider', 'CUDAExecutionProvider', 'CPUExecutionProvider']) # 创建一个推理session
+    input1 = features.get_feature_input()
+    outputs = ort_session.run(None,{ 'input_0' : input1.cpu().numpy()})
     print(outputs)
 
 
@@ -43,15 +50,14 @@ def valid_check(output1: object,output2: object):
     
 @app.command()
 def pt_to_onnx(path: str):
-    model = load_model(path)
-    if model == None:
-        return
-    
+    model= features.get_feature_modes()
     input_names = ["input_0"]
     output_names = ["output_0"]
-    
-    onnx_model = torch.onnx.export(model,input1,path+'onnx',input_names=input_names,output_names=output_names)
-    onnx.checker.check_model(onnx_model)
+    input1 = torch.randn(1, 3, 224, 224)
+    if torch.cuda.is_available():
+        input1 = input1.cuda()
+    torch.onnx.export(model,input1,path+'.onnx',export_params=True,do_constant_folding=True,
+                         verbose = True,input_names=input_names,output_names=output_names,opset_version=11)
     
 def load_model(path: str,where: str = 'jit'):
     model = object()
@@ -70,8 +76,11 @@ def load_model(path: str,where: str = 'jit'):
         model.load_state_dict(checkpoint['model_state_dict'])
     elif where == 'onnx':
         model = onnx.load(path) # 加载onnx
-        if not onnx.checker.check_model(model): # 检查生成模型是否错误
-            return None
+        onnx.checker.check_model(model)
+        print(onnx.helper.printable_graph(model.graph))
+        for _input in model.graph.input:
+            print(MessageToDict(_input))
+            
     return model
 
 @app.command()
@@ -98,8 +107,6 @@ def info(path: str,where: str = 'jit'):
             print(layer.__class__.__name__,"output shape:",x.shape)
             
 def print_model_info(model: object):
-    # print('input shape:',model.inpu)
-    # print('input size:',model.size)
     print('params:',model.parameters())
     
 def image_format(path: str,size,shape):
@@ -117,7 +124,7 @@ def download(git: str,name: str,where: str = 'pytorch'):
     
 @app.command()
 def test():
-    model = features.get_feature_modes()
-    print_model_info(model)
+    print("test")
+    
 if __name__ == '__main__':
     app()

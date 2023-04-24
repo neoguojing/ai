0e2f4fc0151e0bc1ac9b065b934b8c627ff6c9ef
guoerjun <guoerjun@sensetime.com>
2023-04-24 11:50:09 +0800

    up

--- src/class.py
+++ src/class.py
@@ -1,8 +1,9 @@
-
 import torch
 from dataset import imagenet_labels
 import torchvision.models as models
 from torchvision.models import shufflenet_v2_x1_0
+from torchvision.transforms import transforms
+from PIL import Image
 
 # Define the model factory function
 def get_model(model_name):
@@ -11,7 +12,7 @@ def get_model(model_name):
     elif model_name == 'mobilenetv2':
         model = models.mobilenet_v2(pretrained=True)
     elif model_name == 'shufflenetv2':
-        model = models.shufflenet_v2_x1_0(pretrained=True)
+        model = shufflenet_v2_x1_0(pretrained=True)
     else:
         raise ValueError('Invalid model name')
     # Use GPU if available
@@ -22,12 +23,32 @@ def get_model(model_name):
 
 
 # Define the classification function for multi-class classification
-def classification(image,model):
+def classification(image_path, model_name):
+    # Load the image
 
-    output = model(image)
+    # Get the model
+    model = get_model(model_name)
+    # Use the model for inference
+# Load the image
+    image = Image.open(image_path)
+    # Preprocess the image
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
+    tensor = preprocess(image).unsqueeze(0)
+
+    # Use the model for inference
+    output = model(tensor)
+
     # Post-process the output
-    post_processor(output)
-    return output
+    label = post_processor(output)
+    return label
+
+
 
 # Define the post-processor function to convert class result to human read format
 def post_processor(output):
@@ -36,10 +57,5 @@ def post_processor(output):
     # Convert the index to a human-readable label
     label = imagenet_labels[index[0]]
     return label
-
-
-
-
-
 
 

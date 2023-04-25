4a0c3e107990f6e23308abd8edde7201cf6cb6bf
guoerjun <guoerjun@sensetime.com>
2023-04-25 17:22:43 +0800

    up

--- src/tools.py
+++ src/tools.py
@@ -16,3 +16,31 @@ def category_encode(labels):
     encoded_labels = torch.nn.functional.one_hot(labels)
     return encoded_labels
 
+
+def scale_bbox(bbox, scale_factor):
+    """
+    Scales the given bounding box by the given scale factor.
+    """
+    x1, y1, x2, y2 = bbox
+    width = x2 - x1
+    height = y2 - y1
+    new_width = int(width * scale_factor)
+    new_height = int(height * scale_factor)
+    new_x1 = int(x1 - (new_width - width) / 2)
+    new_y1 = int(y1 - (new_height - height) / 2)
+    new_x2 = new_x1 + new_width
+    new_y2 = new_y1 + new_height
+    return (new_x1, new_y1, new_x2, new_y2)
+
+
+def cal_scale_factor(original_size, target_size):
+    """
+    Calculates the scale factor needed to resize an image from its original size to the target size.
+    """
+    original_width, original_height = original_size
+    target_width, target_height = target_size
+    width_scale_factor = target_width / original_width
+    height_scale_factor = target_height / original_height
+    return min(width_scale_factor, height_scale_factor)
+
+

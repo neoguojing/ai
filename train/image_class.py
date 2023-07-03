5bda7c5c56c030da507c3985c995f8963b32a955
guoerjun <guoerjun@sensetime.com>
2023-07-03 16:54:03 +0800

    up

--- train/image_class.py
+++ train/image_class.py
@@ -1,6 +1,43 @@
 import torch
 import torch.nn as nn
 from torchvision import models
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from torch.utils.data import Dataset, DataLoader
+
+# Define transforms for data augmentation and normalization
+train_transform = transforms.Compose([
+    transforms.RandomResizedCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
+
+# Define a custom dataset
+class ImageFolderDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.dataset = datasets.ImageFolder(root_dir, transform=transform)
+        
+    def __len__(self):
+        return len(self.dataset)
+    
+    def __getitem__(self, index):
+        return self.dataset[index]
+
+# Define the directories for the dataset
+data_dirs = ['./data/class1/', './data/class2/', './data/class3/']
+
+# Create a list of datasets
+datasets = [ImageFolderDataset(data_dir, transform=train_transform) for data_dir in data_dirs]
+
+# Merge the datasets into a single dataset
+merged_dataset = torch.utils.data.ConcatDataset(datasets)
+
+# Create a data loader to load the dataset in batches
+batch_size = 32
+dataloader = DataLoader(merged_dataset, batch_size=batch_size, shuffle=True)
+
 
 # Load the pretrained model
 model = models.resnet50(pretrained=True)
@@ -14,12 +51,14 @@ for param in model.layer4.parameters():
     param.requires_grad = True
 
 # Define a new, untrained feed-forward network as a classifier, using ReLU activations and dropout
-classifier = nn.Sequential(nn.Linear(2048, 512),
-                           nn.ReLU(),
-                           nn.Dropout(0.2),
-                           nn.Linear(512, 10), # 10 classes as an example
-                           nn.LogSoftmax(dim=1))
+# classifier = nn.Sequential(nn.Linear(2048, 512),
+#                            nn.ReLU(),
+#                            nn.Dropout(0.2),
+#                            nn.Linear(512, 10), # 10 classes as an example
+#                            nn.LogSoftmax(dim=1))
 
+num_classes = len(data_dirs)
+classifier = torch.nn.Linear(model.fc.in_features, num_classes)
 # Replace the model's classifier with this new classifier
 model.fc = classifier
 
@@ -37,8 +76,9 @@ epochs = 5
 # Start the training loop
 for epoch in range(epochs):
     running_loss = 0
-    for inputs, labels in trainloader: # assuming trainloader is the data loader
+    for batch in dataloader: # assuming trainloader is the data loader
         # Move input and label tensors to the device
+        images, labels = batch
         inputs, labels = inputs.to(device), labels.to(device)
         
         # Zero the parameter gradients
@@ -55,4 +95,4 @@ for epoch in range(epochs):
         running_loss += loss.item()
         
     print(f"Epoch {epoch+1}/{epochs}.. "
-          f"Train loss: {running_loss/len(trainloader):.3f}.. ")
+          f"Train loss: {running_loss/len(dataloader):.3f}.. ")
--- train/text_class.py
+++ train/text_class.py
@@ -1,30 +1,59 @@
-from transformers import AlbertTokenizer, AlbertForSequenceClassification
 import torch
+import sqlite3
+import pandas as pd
+from transformers import AlbertTokenizerFast,AlbertForSequenceClassification,AlbertTokenizer
+from torch.utils.data import Dataset, DataLoader
 
-# Load pre-trained model tokenizer
-tokenizer = AlbertTokenizer.from_pretrained('albert-base-v2')
+# Define a custom dataset
+class TextDataset(Dataset):
+    def __init__(self, data, labels, tokenizer):
+        self.data = data
+        self.labels = labels
+        self.tokenizer = tokenizer
 
-# Load pre-trained model
-model = AlbertForSequenceClassification.from_pretrained('albert-base-v2')
+    def __len__(self):
+        return len(self.data)
 
+    def __getitem__(self, index):
+        encoded_text = self.tokenizer(self.data[index], padding='max_length', truncation=True, max_length=128, return_tensors='pt')
+        label = torch.tensor(self.labels[index])
+        attention_mask = encoded_text['attention_mask']
+        return encoded_text, attention_mask, label
+
+# Connect to the SQLite database and retrieve data and labels
+conn = sqlite3.connect('mydatabase.db')
+cursor = conn.execute("SELECT data, label FROM mytable")
+rows = cursor.fetchall()
+
+# Convert the data and labels to a Pandas DataFrame
+df = pd.DataFrame(rows, columns=['data', 'label'])
+
+# Load the Albert tokenizer
+tokenizer = AlbertTokenizerFast.from_pretrained('albert-base-v2')
+
+# Create an instance of the custom dataset
+dataset = TextDataset(df['data'].tolist(), df['label'].tolist(), tokenizer)
+
+# Create a data loader to load the dataset in batches
+batch_size = 32
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+# Define the Albert model
+model = AlbertForSequenceClassification.from_pretrained('albert-base-v2', num_labels=2)
 # Define the optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-5)
-
 # Define the loss function
 loss_fn = torch.nn.CrossEntropyLoss()
-
-# Define the training loop
-def train(model, optimizer, loss_fn, dataloader, device):
-    model.train()
+num_epochs = 3
+# Train the model with the data loader
+for epoch in range(num_epochs):
     for batch in dataloader:
+        inputs = batch[0]
+        attention_mask = batch[1]
+        labels = batch[2]
+        outputs = model(inputs['input_ids'], attention_mask=attention_mask, labels=labels)
+        loss = outputs.loss
+        # loss = loss_fn(outputs.logits, labels)
         optimizer.zero_grad()
-        input_ids = batch['input_ids'].to(device)
-        attention_mask = batch['attention_mask'].to(device)
-        labels = batch['labels'].to(device)
-        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
-        loss = loss_fn(outputs.logits, labels)
         loss.backward()
         optimizer.step()
-
-# Assume dataloader is defined and device is set
-train(model, optimizer, loss_fn, dataloader, device)

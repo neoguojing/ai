578a4702b0ddde4e51f857d73c116531cd3e1d57
guoerjun <guoerjun@sensetime.com>
2024-05-13 19:41:07 +0800

    up

--- detection2/data/data_loader.py
+++ detection2/data/data_loader.py
@@ -2,46 +2,72 @@ from detectron2.data import DatasetCatalog, MetadataCatalog
 from detectron2.structures import BoxMode
 import os
 import json
-from PIL import Image
 
-def load_detection2_dataset(data_dir):
-    """
-    加载自定义的 detection2 数据集
-    Args:
-        data_dir (str): 数据集根目录
-    Returns:
-        list[dict]: 包含数据集中每个样本信息的列表
-    """
-    dataset_dicts = []
-    # 假设数据集的注释文件是 JSON 格式
-    annotation_file = os.path.join(data_dir, "annotations.json")
+class CustomDataset:
+    def __init__(self, data_dir, dataset_name='my_dataset', thing_classes=None):
+        self.data_dir = data_dir
+        self.dataset_name = dataset_name
+        self.thing_classes = thing_classes
+        self.data = self._load_annotations()
+        self.train_dataset_dicts = []
+        self.val_dataset_dicts = []
 
-    with open(annotation_file) as f:
-        data = json.load(f)
+    def _load_annotations(self):
+        annotation_file = os.path.join(self.data_dir, "annotations.json")
+        with open(annotation_file) as f:
+            data = json.load(f)
+        return data
 
-    for img_data in data['images']:
-        record = {}
-        record["file_name"] = os.path.join(data_dir, img_data["file_name"])
-        record["height"] = img_data["height"]
-        record["width"] = img_data["width"]
-        record["image_id"] = img_data["id"]
+    def _create_image_record(self, img_data, data_dir):
+        record = {
+            "file_name": os.path.join(data_dir, img_data["file_name"]),
+            "height": img_data["height"],
+            "width": img_data["width"],
+            "image_id": img_data["id"],
+            "annotations": []
+        }
+        return record
 
-        objs = []
-        for anno in data['annotations']:
-            if anno['image_id'] == img_data['id']:
+    def _add_annotations_to_record(self, record, annotations):
+        for anno in annotations:
+            if anno['image_id'] == record['image_id']:
                 obj = {
                     "bbox": anno["bbox"],
                     "bbox_mode": BoxMode.XYWH_ABS,
                     "category_id": anno["category_id"]
                 }
-                objs.append(obj)
-        record["annotations"] = objs
-        dataset_dicts.append(record)
+                record["annotations"].append(obj)
 
-    return dataset_dicts
+    def _create_dataset_dicts(self, data_type):
+        dataset_dicts = []
+        data_dir = os.path.join(self.data_dir, data_type)
 
-def register_my_dataset(data_set_name,data_path,thing_classes):
-    for d in ["train", "val"]:
-        DatasetCatalog.register(data_set_name + d, lambda d=d: load_detection2_dataset(data_path + d))
-        MetadataCatalog.get(data_set_name + d).set(thing_classes=thing_classes)
+        for img_data in self.data['images']:
+            record = self._create_image_record(img_data, data_dir)
+            self._add_annotations_to_record(record, self.data['annotations'])
+            dataset_dicts.append(record)
 
+        return dataset_dicts
+
+    def register_datasets(self):
+        for d in ["train", "val"]:
+            dataset_dicts = self._create_dataset_dicts(d)
+            if d == "train":
+                self.train_dataset_dicts = dataset_dicts
+            elif d == "val":
+                self.val_dataset_dicts = dataset_dicts
+
+            DatasetCatalog.register(self.dataset_name + d, lambda d=d: self._create_dataset_dicts(d))
+            MetadataCatalog.get(self.dataset_name + d).set(thing_classes=self.thing_classes)
+
+if __name__ == "__main__":
+    # Example usage:
+    data_dir = "/path/to/your/data/"
+    thing_classes = ["class1", "class2", "class3"]
+    dataset_name = 'my_dataset'
+    custom_dataset = CustomDataset(data_dir, dataset_name, thing_classes)
+    custom_dataset.register_datasets()
+
+    # Access the dataset dicts if needed
+    train_dataset = DatasetCatalog.get(dataset_name + "train")
+    val_dataset = DatasetCatalog.get(dataset_name + "val")
--- detection2/train/train.py
+++ detection2/train/train.py
@@ -1,4 +1,3 @@
-
 import detectron2
 from detectron2.utils.logger import setup_logger
 setup_logger()
@@ -10,29 +9,57 @@ from google.colab.patches import cv2_imshow
 
 # import some common detectron2 utilities
 from detectron2 import model_zoo
-from detectron2.engine import DefaultPredictor
+from detectron2.engine import DefaultPredictor, DefaultTrainer
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
 from detectron2.utils.visualizer import ColorMode
 
-from detectron2.engine import DefaultTrainer
+class ModelTrainer:
+    def __init__(self, model_config_path, class_names, task="object_detection"):
+        """
+        Initializes the trainer for various tasks.
 
-cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.DATASETS.TRAIN = ("balloon_train",)
-cfg.DATASETS.TEST = ()
-cfg.DATALOADER.NUM_WORKERS = 2
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
-cfg.SOLVER.IMS_PER_BATCH = 2  # This is the real "batch size" commonly known to deep learning people
-cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
-cfg.SOLVER.STEPS = []        # do not decay learning rate
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
-# NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
+        Args:
+            model_config_path (str): Path to the model configuration file.
+            class_names (list): List of class names for the dataset.
+            task (str): The type of task to perform, e.g., "object_detection".
+        """
+        self.model_config_path = model_config_path
+        self.class_names = class_names
+        self.task = task
+        self.cfg = get_cfg()
+        self.cfg.merge_from_file(model_config_path)
+        
+        if task == "object_detection":
+            self.cfg.DATASETS.TRAIN = ("balloon_train",)
+            self.cfg.DATASETS.TEST = ()
+            self.cfg.DATALOADER.NUM_WORKERS = 2
+            self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(self.model_config_path)
+            self.cfg.SOLVER.IMS_PER_BATCH = 2
+            self.cfg.SOLVER.BASE_LR = 0.00025
+            self.cfg.SOLVER.MAX_ITER = 300
+            self.cfg.SOLVER.STEPS = []
+            self.cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
+            self.cfg.MODEL.ROI_HEADS.NUM_CLASSES = len(class_names)
+        else:
+            raise ValueError("Unsupported task: {}".format(task))
+        
+        os.makedirs(self.cfg.OUTPUT_DIR, exist_ok=True)
 
-os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-trainer = DefaultTrainer(cfg) 
-trainer.resume_or_load(resume=False)
-trainer.train()
+    def train(self):
+        """
+        Trains the model using the provided configuration.
+        """
+        if self.task == "object_detection":
+            trainer = DefaultTrainer(self.cfg)
+            trainer.resume_or_load(resume=False)
+            trainer.train()
+        else:
+            raise ValueError("Unsupported task: {}".format(self.task))
+
+if __name__ == "__main__":
+    # Use the class to train a object detection model with 2 classes
+    class_names = ["balloon", "person"]
+    trainer = ModelTrainer("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml", class_names, task="object_detection")
+    trainer.train()

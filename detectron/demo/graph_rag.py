c1640f79fcafae11be5dbc0ce2c6389bb49c1f4e
guoerjun <guoerjun@sensetime.com>
2024-09-05 15:36:20 +0800

    fix

--- detectron/demo/graph_rag.py
+++ detectron/demo/graph_rag.py
@@ -1,9 +1,14 @@
-from graphrag.index import index_cli
 import os
-
+from dotenv import load_dotenv
+import shutil
+from pathlib import Path
+import asyncio
+import subprocess
 import pandas as pd
 import tiktoken
-
+from graphrag.query.llm.oai.chat_openai import ChatOpenAI
+from graphrag.query.llm.oai.embedding import OpenAIEmbedding
+from graphrag.query.llm.oai.typing import OpenaiApiType
 from graphrag.query.structured_search.global_search.community_context import (
     GlobalCommunityContext,
 )
@@ -22,9 +27,9 @@ from graphrag.query.input.loaders.dfs import (
     store_entity_semantic_embeddings,
 )
 
-from llm import CustomLLM
+# from llm import CustomLLM
+# from embedding import Embedding
 from graphrag.query.llm.oai.embedding import OpenAIEmbedding
-from embedding import Embedding
 from graphrag.query.llm.oai.typing import OpenaiApiType
 from graphrag.query.question_gen.local_gen import LocalQuestionGen
 from graphrag.query.structured_search.local_search.mixed_context import (
@@ -33,145 +38,194 @@ from graphrag.query.structured_search.local_search.mixed_context import (
 from graphrag.query.structured_search.local_search.search import LocalSearch
 from graphrag.vector_stores.lancedb import LanceDBVectorStore
 
-
-# parquet files generated from indexing pipeline
-INPUT_DIR = "./knowledge_bases/operation dulce"
-COMMUNITY_REPORT_TABLE = "create_final_community_reports"
-ENTITY_TABLE = "create_final_nodes"
-ENTITY_EMBEDDING_TABLE = "create_final_entities"
-
-LANCEDB_URI = f"{INPUT_DIR}/lancedb"
-
-RELATIONSHIP_TABLE = "create_final_relationships"
-COVARIATE_TABLE = "create_final_covariates"
-TEXT_UNIT_TABLE = "create_final_text_units"
-
 # community level in the Leiden community hierarchy from which we will load the community reports
 # higher value means we use reports from more fine-grained communities (at the cost of higher computation cost)
 COMMUNITY_LEVEL = 2
 
+RELATIONSHIP_TABLE = "create_final_relationships"
+COVARIATE_TABLE = "create_final_covariates"
+TEXT_UNIT_TABLE = "create_final_text_units"
+COMMUNITY_REPORT_TABLE = "create_final_community_reports"
+ENTITY_TABLE = "create_final_nodes"
+ENTITY_EMBEDDING_TABLE = "create_final_entities"
+ROOT = "./knowledge_bases"
+ENV_DIR = f"{ROOT}/.env"
+SETTING_DIR = f"{ROOT}/settings.yaml"
 class GraphRag:
-    llm = CustomLLM()   
+    
+    def __init__(self,base_name: str):
+        # parquet files generated from indexing pipeline
+        self.BASE_DIR = f"{ROOT}/{base_name}"
+        self.INPUT_DIR = f"{self.BASE_DIR}/operation dulce"
+        self.LANCEDB_URI = f"{self.INPUT_DIR}/lancedb"
+        self.prepare_env()
+        # 显式指定 .env 文件路径
+        load_dotenv(dotenv_path=f"{self.BASE_DIR}/.env")
+        self.api_key = os.environ["GRAPHRAG_API_KEY"]
+        self.llm_model = os.environ["GRAPHRAG_LLM_MODEL"]
+        self.embedding_model = os.environ["GRAPHRAG_EMBEDDING_MODEL"]
+        self.api_base = os.environ["GRAPHRAG_API_BASE"]
+        self.llm = ChatOpenAI(
+            api_key=self.api_key,
+            model=self.llm_model,
+            api_type=OpenaiApiType.OpenAI,  # OpenaiApiType.OpenAI or OpenaiApiType.AzureOpenAI
+            max_retries=20,
+            api_base=self.api_base,
+        )
+        
+        self.token_encoder = tiktoken.get_encoding("cl100k_base")
 
-    token_encoder = tiktoken.get_encoding("cl100k_base")
+        # self.text_embedder = Embedding()
+        self.text_embedder = OpenAIEmbedding(
+            api_key=self.api_key,
+            api_base=self.api_base,
+            api_type=OpenaiApiType.OpenAI,
+            model=self.embedding_model,
+            deployment_name=self.embedding_model,
+            max_retries=20,
+        )
 
-    text_embedder = Embedding()
+        self.load_graph()
 
-    # read nodes table to get community and degree data
-    entity_df = pd.read_parquet(f"{INPUT_DIR}/{ENTITY_TABLE}.parquet")
-    entity_embedding_df = pd.read_parquet(f"{INPUT_DIR}/{ENTITY_EMBEDDING_TABLE}.parquet")
-    entities = read_indexer_entities(entity_df, entity_embedding_df, COMMUNITY_LEVEL)
+    def load_graph(self):
+        if not any(Path(self.INPUT_DIR).glob("*.parquet")):
+            print(f"no .parquet files  in  {self.INPUT_DIR}")
+            return 
 
-    # load description embeddings to an in-memory lancedb vectorstore
-    # to connect to a remote db, specify url and port values.
-    description_embedding_store = LanceDBVectorStore(
-        collection_name="entity_description_embeddings",
-    )
-    description_embedding_store.connect(db_uri=LANCEDB_URI)
-    entity_description_embeddings = store_entity_semantic_embeddings(
-        entities=entities, vectorstore=description_embedding_store
-    )
+         # read nodes table to get community and degree data
+        self.entity_df = pd.read_parquet(f"{self.INPUT_DIR}/{ENTITY_TABLE}.parquet")
+        self.entity_embedding_df = pd.read_parquet(f"{self.INPUT_DIR}/{ENTITY_EMBEDDING_TABLE}.parquet")
+        self.entities = read_indexer_entities(self.entity_df, self.entity_embedding_df, COMMUNITY_LEVEL)
 
-    relationship_df = pd.read_parquet(f"{INPUT_DIR}/{RELATIONSHIP_TABLE}.parquet")
-    relationships = read_indexer_relationships(relationship_df)
+        # load description embeddings to an in-memory lancedb vectorstore
+        # to connect to a remote db, specify url and port values.
+        self.description_embedding_store = LanceDBVectorStore(
+            collection_name="entity_description_embeddings",
+        )
+        self.description_embedding_store.connect(db_uri=self.LANCEDB_URI)
+        self.entity_description_embeddings = store_entity_semantic_embeddings(
+            entities=self.entities, vectorstore=self.description_embedding_store
+        )
+        print(f"Entity count: {len(self.entity_df)}")
+        self.entity_df.head()
 
-    covariate_df = pd.read_parquet(f"{INPUT_DIR}/{COVARIATE_TABLE}.parquet")
-    claims = read_indexer_covariates(covariate_df)
-    covariates = {"claims": claims}
+        self.relationship_df = pd.read_parquet(f"{self.INPUT_DIR}/{RELATIONSHIP_TABLE}.parquet")
+        self.relationships = read_indexer_relationships(self.relationship_df)
+        print(f"Relationship count: {len(self.relationship_df)}")
+        self.relationship_df.head()
 
-    report_df = pd.read_parquet(f"{INPUT_DIR}/{COMMUNITY_REPORT_TABLE}.parquet")
-    reports = read_indexer_reports(report_df, entity_df, COMMUNITY_LEVEL)
+        self.covariate_df = pd.read_parquet(f"{self.INPUT_DIR}/{COVARIATE_TABLE}.parquet")
+        self.claims = read_indexer_covariates(self.covariate_df)
+        print(f"Claim records: {len(self.claims)}")
+        self.covariates = {"claims": self.claims}
+        
+        self.report_df = pd.read_parquet(f"{self.INPUT_DIR}/{COMMUNITY_REPORT_TABLE}.parquet")
+        self.reports = read_indexer_reports(self.report_df, self.entity_df, COMMUNITY_LEVEL)
+        print(f"Report records: {len(self.report_df)}")
+        self.report_df.head()
 
-    text_unit_df = pd.read_parquet(f"{INPUT_DIR}/{TEXT_UNIT_TABLE}.parquet")
-    text_units = read_indexer_text_units(text_unit_df)
+        text_unit_df = pd.read_parquet(f"{self.INPUT_DIR}/{TEXT_UNIT_TABLE}.parquet")
+        self.text_units = read_indexer_text_units(text_unit_df)
+        print(f"Text unit records: {len(self.text_unit_df)}")
+        self.text_unit_df.head()
 
+        self.init_global()
+        self.init_local()
 
-    context_builder = GlobalCommunityContext(
-        community_reports=reports,
-        entities=entities,  # default to None if you don't want to use community weights for ranking
-        token_encoder=token_encoder,
-    )
+    def init_local(self):
+        self.llm_params = {
+            "max_tokens": 2_000,  # change this based on the token limit you have on your model (if you are using a model with 8k limit, a good setting could be 1000=1500)
+            "temperature": 0.0,
+        }
+        
+        self.local_context_builder = LocalSearchMixedContext(
+            community_reports=self.reports,
+            text_units=self.text_units,
+            entities=self.entities,
+            relationships=self.relationships,
+            covariates=self.covariates,
+            entity_text_embeddings=self.description_embedding_store,
+            embedding_vectorstore_key=EntityVectorStoreKey.ID,  # if the vectorstore uses entity title as ids, set this to EntityVectorStoreKey.TITLE
+            text_embedder=self.text_embedder,
+            token_encoder=self.token_encoder,
+        )
 
-    context_builder_params = {
-        "use_community_summary": False,  # False means using full community reports. True means using community short summaries.
-        "shuffle_data": True,
-        "include_community_rank": True,
-        "min_community_rank": 0,
-        "community_rank_name": "rank",
-        "include_community_weight": True,
-        "community_weight_name": "occurrence weight",
-        "normalize_community_weight": True,
-        "max_tokens": 12_000,  # change this based on the token limit you have on your model (if you are using a model with 8k limit, a good setting could be 5000)
-        "context_name": "Reports",
-    }
+        self.local_context_params = {
+            "text_unit_prop": 0.5,
+            "community_prop": 0.1,
+            "conversation_history_max_turns": 5,
+            "conversation_history_user_turns_only": True,
+            "top_k_mapped_entities": 10,
+            "top_k_relationships": 10,
+            "include_entity_rank": True,
+            "include_relationship_weight": True,
+            "include_community_rank": False,
+            "return_candidate_context": False,
+            "embedding_vectorstore_key": EntityVectorStoreKey.ID,  # set this to EntityVectorStoreKey.TITLE if the vectorstore uses entity title as ids
+            "max_tokens": 12_000,  # change this based on the token limit you have on your model (if you are using a model with 8k limit, a good setting could be 5000)
+        }
 
-    context_builder = LocalSearchMixedContext(
-        community_reports=reports,
-        text_units=text_units,
-        entities=entities,
-        relationships=relationships,
-        covariates=covariates,
-        entity_text_embeddings=description_embedding_store,
-        embedding_vectorstore_key=EntityVectorStoreKey.ID,  # if the vectorstore uses entity title as ids, set this to EntityVectorStoreKey.TITLE
-        text_embedder=text_embedder,
-        token_encoder=token_encoder,
-    )
+        self.local_search_engine = LocalSearch(
+            llm=self.llm,
+            context_builder=self.local_context_builder,
+            token_encoder=self.token_encoder,
+            llm_params=self.llm_params,
+            context_builder_params=self.local_context_params,
+            response_type="multiple paragraphs",  # free form text describing the response type and format, can be anything, e.g. prioritized list, single paragraph, multiple paragraphs, multiple-page report
+        )
 
+        self.question_generator = LocalQuestionGen(
+            llm=self.llm,
+            context_builder=self.context_builder,
+            token_encoder=self.token_encoder,
+            llm_params=self.llm_params,
+            context_builder_params=self.local_context_params,
+        )
+        
+    def init_global(self):
+        self.context_builder = GlobalCommunityContext(
+            community_reports=self.reports,
+            entities=self.entities,  # default to None if you don't want to use community weights for ranking
+            token_encoder=self.token_encoder,
+        )
 
-    local_context_params = {
-        "text_unit_prop": 0.5,
-        "community_prop": 0.1,
-        "conversation_history_max_turns": 5,
-        "conversation_history_user_turns_only": True,
-        "top_k_mapped_entities": 10,
-        "top_k_relationships": 10,
-        "include_entity_rank": True,
-        "include_relationship_weight": True,
-        "include_community_rank": False,
-        "return_candidate_context": False,
-        "embedding_vectorstore_key": EntityVectorStoreKey.ID,  # set this to EntityVectorStoreKey.TITLE if the vectorstore uses entity title as ids
-        "max_tokens": 12_000,  # change this based on the token limit you have on your model (if you are using a model with 8k limit, a good setting could be 5000)
-    }
+        self.context_builder_params = {
+            "use_community_summary": False,  # False means using full community reports. True means using community short summaries.
+            "shuffle_data": True,
+            "include_community_rank": True,
+            "min_community_rank": 0,
+            "community_rank_name": "rank",
+            "include_community_weight": True,
+            "community_weight_name": "occurrence weight",
+            "normalize_community_weight": True,
+            "max_tokens": 12_000,  # change this based on the token limit you have on your model (if you are using a model with 8k limit, a good setting could be 5000)
+            "context_name": "Reports",
+        }
 
-    llm_params = {
-        "max_tokens": 2_000,  # change this based on the token limit you have on your model (if you are using a model with 8k limit, a good setting could be 1000=1500)
-        "temperature": 0.0,
-    }
+        self.map_llm_params = {
+            "max_tokens": 1000,
+            "temperature": 0.0,
+            "response_format": {"type": "json_object"},
+        }
 
-    map_llm_params = {
-        "max_tokens": 1000,
-        "temperature": 0.0,
-        "response_format": {"type": "json_object"},
-    }
+        self.reduce_llm_params = {
+            "max_tokens": 2000,  # change this based on the token limit you have on your model (if you are using a model with 8k limit, a good setting could be 1000-1500)
+            "temperature": 0.0,
+        }
 
-    reduce_llm_params = {
-        "max_tokens": 2000,  # change this based on the token limit you have on your model (if you are using a model with 8k limit, a good setting could be 1000-1500)
-        "temperature": 0.0,
-    }
-
-
-    search_engine = GlobalSearch(
-        llm=llm,
-        context_builder=context_builder,
-        token_encoder=token_encoder,
-        max_data_tokens=12_000,  # change this based on the token limit you have on your model (if you are using a model with 8k limit, a good setting could be 5000)
-        map_llm_params=map_llm_params,
-        reduce_llm_params=reduce_llm_params,
-        allow_general_knowledge=False,  # set this to True will add instruction to encourage the LLM to incorporate general knowledge in the response, which may increase hallucinations, but could be useful in some use cases.
-        json_mode=True,  # set this to False if your LLM model does not support JSON mode.
-        context_builder_params=context_builder_params,
-        concurrent_coroutines=32,
-        response_type="multiple paragraphs",  # free form text describing the response type and format, can be anything, e.g. prioritized list, single paragraph, multiple paragraphs, multiple-page report
-    )
-
-    local_search_engine = LocalSearch(
-        llm=llm,
-        context_builder=context_builder,
-        token_encoder=token_encoder,
-        llm_params=llm_params,
-        context_builder_params=local_context_params,
-        response_type="multiple paragraphs",  # free form text describing the response type and format, can be anything, e.g. prioritized list, single paragraph, multiple paragraphs, multiple-page report
-    )
+        self.search_engine = GlobalSearch(
+            llm=self.llm,
+            context_builder=self.context_builder,
+            token_encoder=self.token_encoder,
+            max_data_tokens=12_000,  # change this based on the token limit you have on your model (if you are using a model with 8k limit, a good setting could be 5000)
+            map_llm_params=self.map_llm_params,
+            reduce_llm_params=self.reduce_llm_params,
+            allow_general_knowledge=False,  # set this to True will add instruction to encourage the LLM to incorporate general knowledge in the response, which may increase hallucinations, but could be useful in some use cases.
+            json_mode=True,  # set this to False if your LLM model does not support JSON mode.
+            context_builder_params=self.context_builder_params,
+            concurrent_coroutines=32,
+            response_type="multiple paragraphs",  # free form text describing the response type and format, can be anything, e.g. prioritized list, single paragraph, multiple paragraphs, multiple-page report
+        )
 
     async def g_search(self,input):
         result = await self.search_engine.asearch(
@@ -195,13 +249,6 @@ class GraphRag:
             print(result.context_data["claims"].head())
         print(result.response)
 
-    question_generator = LocalQuestionGen(
-            llm=llm,
-            context_builder=context_builder,
-            token_encoder=token_encoder,
-            llm_params=llm_params,
-            context_builder_params=local_context_params,
-        )
     
     async def q_gene(self):
         question_history = [
@@ -213,3 +260,43 @@ class GraphRag:
         )
 
         return candidate_questions.response
+    
+    def prepare_env(self):
+         # 创建目录，如果目录不存在则创建
+        Path(self.BASE_DIR).mkdir(parents=True, exist_ok=True)
+
+        command = ['python', '-m', 'graphrag.index','--init', '--root', self.BASE_DIR]
+        try:
+            result = subprocess.run(command, check=True, capture_output=True, text=True)
+            print(f"Command executed successfully: {result.stdout}")
+        except subprocess.CalledProcessError as e:
+            print(f"Command failed with error: {e.stderr}")
+
+        # 复制文件到目标目录
+        shutil.copy(ENV_DIR, self.BASE_DIR)
+        print(f"Copied {ENV_DIR} to {self.BASE_DIR}")
+        shutil.copy(SETTING_DIR, self.BASE_DIR)
+        print(f"Copied {SETTING_DIR} to {self.BASE_DIR}")
+
+    async def do_index(self):
+        # 定义要执行的命令
+        command = ['python', '-m', 'graphrag.index', '--root', self.BASE_DIR]
+        
+        # 使用 asyncio.create_subprocess_exec() 异步执行命令
+        process = await asyncio.create_subprocess_exec(
+            *command,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE
+        )
+        
+        # 等待命令完成，并获取输出和错误信息
+        stdout, stderr = await process.communicate()
+        # 打印命令的输出
+        if process.returncode == 0:
+            print(f"Command executed successfully: {stdout.decode()}")
+            self.load_graph()
+        else:
+            print(f"Command failed with error: {stderr.decode()}")
+
+if __name__ == "__main__":
+    rag = GraphRag("test")
